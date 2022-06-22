1d8fe262df60f8c78f0e40fe04f2b5c38e9b2dc6
SanaUdoekong <udoekongsana@gmail.com>
2022-06-22 20:33:08 +0100

    feat: created calculator

---
+++ script.js
@@ -0,0 +1,42 @@
+let num1 = Number(prompt("Enter the first number: "));
+let num2 = Number(prompt("Enter the second number: "));
+let op = prompt("Choose an operation[+, -, *, /]:");
+
+let ans = "Syntax Error!";
+
+function getAnswer(num1, num2, op) {
+    if(op == '+') {
+        ans = num1 + num2;
+        if(isNaN(ans) == true) {
+            alert("Please ensure you enter integers!");
+        } else {
+            alert("The answer is: " + ans + "!");
+        }
+    } else if (op == '-') {
+        ans = num1 - num2;
+        if(isNaN(ans) == true) {
+            alert("Please ensure you enter integers!");
+        } else {
+            alert("The answer is: " + ans + "!");
+        }
+    } else if (op == '*') {
+        ans = num1 * num2;
+        if(isNaN(ans) == true) {
+            alert("Please ensure you enter integers!");
+        } else {
+            alert("The answer is: " + ans + "!");
+        }
+    } else if (op == '/') {
+        ans = num1 / num2;
+        if(isNaN(ans) == true) {
+            alert("Please ensure you enter integers!");
+        } else {
+            alert("The answer is: " + ans + "!");
+        }
+    } else {
+        alert("Syntax Error!");
+    }
+
+
+    getAnswer(num1, num2, op);
+}
